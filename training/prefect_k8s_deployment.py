57a1d0f9ec9790ee040270be7ec17d3b3c97f39d
Antonio Manuel Burgueño Romero <ambrbr@uma.es>
2023-07-04 08:21:38 +0000

    Fixed agent/job resources

--- training/prefect_k8s_deployment.py
+++ training/prefect_k8s_deployment.py
@@ -11,11 +11,36 @@ environment = {
         'FSSPEC_S3_ENDPOINT_URL': 'http://<S3-IP>:<S3-PORT>',
         'AWS_ACCESS_KEY_ID': s3_block.aws_access_key_id.get_secret_value(),
         'AWS_SECRET_ACCESS_KEY': s3_block.aws_secret_access_key.get_secret_value(),
+        'MLFLOW_TRACKING_URI': 'http://<CLUSTER-IP>:<MLFLOW-PORT>',
         'MLFLOW_S3_ENDPOINT_URL': "http://<S3-IP>:<S3-PORT>",
         'MLFLOW_S3_IGNORE_TLS': "true"
     }
 
 environment = dict(settings) | environment
+
+customizations = [
+    {
+        "op": "add",
+        "path": "/spec/template/spec/containers/0/resources",
+        "value": {
+            "requests": {
+                "cpu": "2",
+                "memory": "30gi"
+            },
+            "limits": {
+                "cpu": "2",
+                "memory": "30Gi"
+            }
+        },
+    },{
+        "op": "replace",
+        "path": "/spec/template/spec/parallelism",
+        "value": 5,
+    },{
+        "op": "remove",
+        "path": "/spec/template/spec/completions",
+    }
+]
 
 infra_k8s = KubernetesJob(
     env=environment,
@@ -24,6 +49,7 @@ infra_k8s = KubernetesJob(
     image_pull_policy="Always",
     cluster_config=cluster_config_block,
     job=KubernetesJob.base_job_manifest(),
+    customizations=customizations,
     finished_job_ttl=600
 )
 
